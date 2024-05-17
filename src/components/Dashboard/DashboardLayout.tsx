9892eb9b49ea70ad4419d5e233f0de10678f1f6a
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-05-18 00:33:38 +0100

    metrivs for dashboard added

--- src/components/Dashboard/DashboardLayout.tsx
+++ src/components/Dashboard/DashboardLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import TopBarDashboard from '../shared/TopBarDashboard'
+import TopMetric from '../shared/TopMetric'
 
 const DashboardLayout = () => {
   return (
     <div>
       <TopBarDashboard/>
+      <TopMetric/>
       <div>
         <Outlet/>
       </div>
--- src/components/shared/TopMetric.tsx
+++ src/components/shared/TopMetric.tsx
@@ -1,8 +1,47 @@
+import {
+    Card,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
 
-
+  
 const TopMetric = () => {
   return (
-    <div>TopMetric</div>
+    <div className="flex items-center justify-center flex-row space-x-10 mt-4">
+        <Card className="w-[292.75px]">
+            <CardHeader>
+            <CardDescription>Token Deposits</CardDescription>
+                <CardTitle>50 ETH</CardTitle>
+            </CardHeader>
+        </Card>
+
+        <Card className="w-[292.75px]">
+            <CardHeader>
+                <div className="flex flex-row space-x-4  items-center ">
+                    <CardDescription>Token Deposits</CardDescription>
+                    <img
+                    src="./assets/download.svg"
+                    />
+                </div>
+                <CardTitle>10</CardTitle>
+            </CardHeader>
+        </Card>
+        
+        <Card className="w-[292.75px]">
+            <CardHeader>
+            <CardDescription>Token Balance</CardDescription>
+                <CardTitle>2.00457 BTC</CardTitle>
+            </CardHeader>
+        </Card>
+        
+        <Card className="w-[292.75px]">
+            <CardHeader>
+            <CardDescription>Volume Generated</CardDescription>
+                <CardTitle>$67.897M</CardTitle>
+            </CardHeader>
+        </Card>
+    </div>
   )
 }
 
